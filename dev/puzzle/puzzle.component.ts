b0d0c25267b5e1ae8fe0f0a56532ace895982fd9
Simon Temple <simonctemple@live.com>
2016-03-24 15:14:54 -0700

    finish puzzle project

--- dev/puzzle/puzzle.component.ts
+++ dev/puzzle/puzzle.component.ts
@@ -1,28 +1,64 @@
 import {Component} from 'angular2/core';
+import {OnInit} from "angular2/core";
 
 @Component({
   selector: 'my-puzzle',
   template: `
-    <section class="setup"> 
+    <section class="setup">
+    <h2>Game Setup</h2>
       Enter your name please:
-      <input type="text">
+      <input type="text" #name (keyup)="0">
     </section>
-    <section>
-      <h2>The Puzzle</h2>
-      <p>Ok,  welcome <span class="name">XXX</span></p>
+    <section
+    [ngClass]="{
+      puzzle: true,
+      solved:
+      switch1.value == switch1Number &&
+      switch2.value == switch2Number &&
+      switch3.value == switch3Number &&
+      switch4.value == switch4Number
+    }"
+    [ngStyle]="{display: name.value === '' ? 'none' : 'block'}">
+      <h2>
+      The Puzzle |
+      {{switch1.value == switch1Number &&
+      switch2.value == switch2Number &&
+      switch3.value == switch3Number &&
+      switch4.value == switch4Number ? 'SOLVED' : 'NOT SOLVED'}}
+      </h2>
+      <p>Ok,  welcome <span class="name">{{name.value}}</span></p>
       <br>
       Switch1:
-      <input type="text"><br>
+      <input type="text" #switch1 (keyup)="0"><br>
       Switch2:
-      <input type="text"><br>
+      <input type="text"  #switch2 (keyup)="0"><br>
       Switch3:
-      <input type="text"><br>
+      <input type="text"  #switch3 (keyup)="0"><br>
       Switch4:
-      <input type="text"><br>
+      <input type="text"  #switch4 (keyup)="0"><br>
   </section>
+  <h2 [hidden]="switch1.value != switch1Number ||
+  switch2.value != switch2Number ||
+  switch3.value != switch3Number ||
+  switch4.value != switch4Number">Congratulations {{name.value}}, you did it!<h2>
   `
 })
 
 export class PuzzleComponent {
+  switch1Number: number;
+  switch2Number: number;
+  switch3Number: number;
+  switch4Number: number;
 
+  ngOnInit() {
+    this.switch1Number = Math.round(Math.random());
+    this.switch2Number = Math.round(Math.random());
+    this.switch3Number = Math.round(Math.random());
+    this.switch4Number = Math.round(Math.random());
+
+    console.log(this.switch1Number);
+    console.log(this.switch2Number);
+    console.log(this.switch3Number);
+    console.log(this.switch4Number);
+  }
 }
